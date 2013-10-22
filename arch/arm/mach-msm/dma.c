a6a020862ae5d2183551271832b1c12df4207eae
Flemmard <flemmard@gmail.com>
2013-10-22 17:28:10 +0200

    7x30: dma: some devices doesn't have the proper clocks, ignore them

--- arch/arm/mach-msm/dma.c
+++ arch/arm/mach-msm/dma.c
@@ -631,6 +631,7 @@ static int msm_dmov_init_clocks(struct platform_device *pdev)
 		return -ENOENT;
 	}
 
+#ifndef CONFIG_ARCH_MSM7X30
 	dmov_conf[adm].pclk = clk_get(&pdev->dev, "iface_clk");
 	if (IS_ERR(dmov_conf[adm].pclk)) {
 		dmov_conf[adm].pclk = NULL;
@@ -646,6 +647,7 @@ static int msm_dmov_init_clocks(struct platform_device *pdev)
 		if (ret)
 			return -ENOENT;
 	}
+#endif
 
 	return 0;
 }
