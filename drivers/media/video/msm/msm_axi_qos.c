861665ec25e363e83391ecd205d396aedc15ef89
trostanski <rostan@podkowa.net>
2013-12-06 14:45:49 +0100

    camera: use clock based axi qos

--- drivers/media/video/msm/msm_axi_qos.c
+++ drivers/media/video/msm/msm_axi_qos.c
@@ -9,53 +9,42 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301, USA.
- *
  */
 
-//#include <linux/pm_qos_params.h>
+#include <linux/clk.h>
 #include <mach/camera.h>
 #define MSM_AXI_QOS_NAME "msm_camera"
 
-static struct pm_qos_request_list *pm_qos_req;
+static struct clk *ebi1_clk;
 
 int add_axi_qos(void)
 {
-#if 0
-	pm_qos_req = pm_qos_add_request(PM_QOS_SYSTEM_BUS_FREQ,
-					PM_QOS_DEFAULT_VALUE);
-	if (!pm_qos_req) {
-		CDBG("request AXI bus QOS fails.\n");
-		return -1;
+	ebi1_clk = clk_get(NULL, "ebi1_vfe_clk");
+	if (IS_ERR(ebi1_clk))
+		ebi1_clk = NULL;
+	else {
+		clk_prepare(ebi1_clk);
+		clk_enable(ebi1_clk);
 	}
-#endif
-CDBG("request AXI bus QOS succeed.\n");
+
 	return 0;
 }
 
 int update_axi_qos(uint32_t rate)
 {
-#if 0
-	if (!pm_qos_req) {
-		CDBG("add_axi_qos() has not been called\n");
-		return -1;
-	}
+	if (!ebi1_clk)
+		return 0;
 
-	pm_qos_update_request(pm_qos_req, rate);
-#endif
-	return 0;
+	return clk_set_rate(ebi1_clk, rate * 1000);
 }
 
 void release_axi_qos(void)
 {
-#if 0
-	if (!pm_qos_req) {
-		CDBG("add_axi_qos() has not been called\n");
-	}
+	if (!ebi1_clk)
+		return;
 
-	pm_qos_remove_request(pm_qos_req);
-#endif
+	clk_disable(ebi1_clk);
+	clk_unprepare(ebi1_clk);
+	clk_put(ebi1_clk);
+	ebi1_clk = NULL;
 }
