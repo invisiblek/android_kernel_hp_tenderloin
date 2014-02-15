1c6207f28243938c1b650f70664942635842e223
Dan Pasanen <dan.pasanen@gmail.com>
2014-02-15 12:17:39 -0600

    slub: fix a warning

--- mm/slub.c
+++ mm/slub.c
@@ -4526,9 +4526,11 @@ static ssize_t show_slab_objects(struct kmem_cache *s,
 				else if (flags & SO_OBJECTS)
 					WARN_ON_ONCE(1);
 				else
+				{
 					x = page->pages;
-				total += x;
-				nodes[node] += x;
+					total += x;
+					nodes[node] += x;
+				}
 			}
 			per_cpu[node]++;
 		}
