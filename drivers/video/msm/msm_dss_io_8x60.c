db516c0ab0682c6ec53dca2b0d873692ebd7a75e
Flemmard <flemmard@gmail.com>
2014-02-21 23:24:15 +0100

    video: msm: update msm_dss_io_8x60

--- drivers/video/msm/msm_dss_io_8x60.c
+++ drivers/video/msm/msm_dss_io_8x60.c
@@ -410,22 +410,29 @@ void cont_splash_clk_ctrl(int enable)
 {
 }
 
-void mipi_dsi_prepare_clocks(void)
+void mipi_dsi_prepare_ahb_clocks(void)
 {
 	clk_prepare(amp_pclk);
 	clk_prepare(dsi_m_pclk);
 	clk_prepare(dsi_s_pclk);
-	clk_prepare(dsi_byte_div_clk);
-	clk_prepare(dsi_esc_clk);
 }
+//	clk_prepare(dsi_byte_div_clk);
+//	clk_prepare(dsi_esc_clk);
 
+void mipi_dsi_unprepare_ahb_clocks(void)
+{
+	clk_unprepare(dsi_m_pclk);
+	clk_unprepare(dsi_s_pclk);
+	clk_unprepare(amp_pclk);
+}
 void mipi_dsi_unprepare_clocks(void)
 {
 	clk_unprepare(dsi_esc_clk);
 	clk_unprepare(dsi_byte_div_clk);
-	clk_unprepare(dsi_m_pclk);
-	clk_unprepare(dsi_s_pclk);
-	clk_unprepare(amp_pclk);
+}
+
+void mipi_dsi_configure_fb_divider(u32 fps_level)
+{
 }
 
 void mipi_dsi_ahb_ctrl(u32 enable)
@@ -468,8 +475,8 @@ void mipi_dsi_clk_enable(void)
 		pr_err("%s: clk_set_rate failed\n",	__func__);
 	mipi_dsi_pclk_ctrl(&dsi_pclk, 1);
 	mipi_dsi_clk_ctrl(&dsicore_clk, 1);
-	clk_enable(dsi_byte_div_clk);
-	clk_enable(dsi_esc_clk);
+	clk_prepare_enable(dsi_byte_div_clk);
+	clk_prepare_enable(dsi_esc_clk);
 	mipi_dsi_clk_on = 1;
 }
 
@@ -643,11 +650,20 @@ void hdmi_msm_powerdown_phy(void)
 	udelay(10);
 	/* Disable PLL */
 	HDMI_OUTP_ND(0x030C, 0x00);
+
+#ifdef WORKAROUND_FOR_HDMI_CURRENT_LEAKAGE_FIX
+	HDMI_OUTP_ND(0x02D4, 0x4);	//Assert RESET PHY from controller
+	udelay(10);
+	HDMI_OUTP_ND(0x02D4, 0x0);	//De-assert RESET PHY from controller
+	HDMI_OUTP_ND(0x0308, 0x1F); //Turn off Driver
+	udelay(10);
+#endif
+
 	/* Power down PHY */
 	HDMI_OUTP_ND(0x0308, 0x7F); /*0b01111111*/
 }
 
-void hdmi_frame_ctrl_cfg(const struct hdmi_disp_mode_timing_type *timing)
+void hdmi_frame_ctrl_cfg(const struct msm_hdmi_mode_timing_info *timing)
 {
 	/*  0x02C8 HDMI_FRAME_CTRL
 	 *  31 INTERLACED_EN   Interlaced or progressive enable bit
