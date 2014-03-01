1ad318b54bfccd09a3ef84f0b2bd827e9a3e4c6b
Flemmard <flemmard@gmail.com>
2014-03-01 15:53:12 +0100

    pyramid: update boards for camera

--- arch/arm/mach-msm/htc/pyramid/board-pyramid.h
+++ arch/arm/mach-msm/htc/pyramid/board-pyramid.h
@@ -136,6 +136,13 @@
 #define PYRAMID_CAM_CAM1_ID           (10)
 #define PYRAMID_CAM_I2C_SDA           (47)
 #define PYRAMID_CAM_I2C_SCL           (48)
+#define PYRAMID_CAM_MCLK     	 (32)
+#define PYRAMID_CAM_VCM_PD      (58)
+#define PYRAMID_CAM1_RSTz       (137)
+#define PYRAMID_CAM2_RSTz       (138)
+#define PYRAMID_CAM2_PWDN       (140)
+#define PYRAMID_MCLK_SWITCH     (141)
+
 
 /* General */
 #define PYRAMID_GENERAL_I2C_SDA		(59)
