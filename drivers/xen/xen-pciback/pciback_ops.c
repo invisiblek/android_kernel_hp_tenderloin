12103cb432eba7570d4f50c05df3933da2ae4e6b
Dan Carpenter <dan.carpenter@oracle.com>
2014-03-28 11:24:59 +0300

    xen-pciback: silence an unwanted debug printk
    There is a missing curly brace here so we might print some extra debug
    information.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: David Vrabel <david.vrabel@citrix.com>

--- drivers/xen/xen-pciback/pciback_ops.c
+++ drivers/xen/xen-pciback/pciback_ops.c
@@ -216,7 +216,7 @@ int xen_pcibk_enable_msix(struct xen_pcibk_device *pdev,
 	if (result == 0) {
 		for (i = 0; i < op->value; i++) {
 			op->msix_entries[i].entry = entries[i].entry;
-			if (entries[i].vector)
+			if (entries[i].vector) {
 				op->msix_entries[i].vector =
 					xen_pirq_from_irq(entries[i].vector);
 				if (unlikely(verbose_request))
@@ -224,6 +224,7 @@ int xen_pcibk_enable_msix(struct xen_pcibk_device *pdev,
 						"MSI-X[%d]: %d\n",
 						pci_name(dev), i,
 						op->msix_entries[i].vector);
+			}
 		}
 	} else
 		pr_warn_ratelimited(DRV_NAME ": %s: error enabling MSI-X for guest %u: err %d!\n",
