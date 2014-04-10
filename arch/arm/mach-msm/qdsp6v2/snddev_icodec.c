ff30ebbd96e48aa9e591252e3907f3782f95f142
milaq <micha.laqua@gmail.com>
2014-04-10 23:26:16 +0200

    audio: bring back icodec rx idlelock hold hack
    holds a idlelock while icodec rx (audio_out) is
    active to ensure no q6 watchdogs bites happen in
    suspend. This allows us to reenable power collapse
    for core 0. This is a port from 3.0 kernel as 3.4
    uses pm_qos_update_request.
    
    Change-Id: Ic456ea1a97602a9d044ed4f80bc785a934263bfc

--- arch/arm/mach-msm/qdsp6v2/snddev_icodec.c
+++ arch/arm/mach-msm/qdsp6v2/snddev_icodec.c
@@ -564,7 +564,7 @@ static int snddev_icodec_open_tx(struct snddev_icodec_state *icodec)
 
 	icodec->enabled = 1;
 
-	//pm_qos_update_request(&drv->tx_pm_qos_req, PM_QOS_DEFAULT_VALUE);
+	pm_qos_update_request(&drv->tx_pm_qos_req, PM_QOS_DEFAULT_VALUE);
 	return 0;
 
 error_invalid_freq:
@@ -652,7 +652,7 @@ static int snddev_icodec_close_rx(struct snddev_icodec_state *icodec)
 
 	icodec->enabled = 0;
 
-	pm_qos_update_request(&drv->rx_pm_qos_req, PM_QOS_DEFAULT_VALUE);
+	//pm_qos_update_request(&drv->rx_pm_qos_req, PM_QOS_DEFAULT_VALUE);
 	return 0;
 }
 
