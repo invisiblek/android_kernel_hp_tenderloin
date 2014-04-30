3aa8a0afc14b3cde2584e4125112a152a08bd969
Flemmard <flemmard@gmail.com>
2014-04-30 19:15:54 +0200

    Revert "acpuclock-8x60: set default frequencies up to 1512(fast)"
    This reverts commit 164527d085c43175e2ae7693910ee8070d46d281.

--- arch/arm/mach-msm/acpuclock-8x60.c
+++ arch/arm/mach-msm/acpuclock-8x60.c
@@ -1052,7 +1052,6 @@ static __init struct clkctl_acpu_speed *select_freq_plan(void)
 		}
 	} else {
 		acpu_freq_tbl = acpu_freq_tbl_1188mhz;
-		acpu_freq_tbl = acpu_freq_tbl_1512mhz_fast;
 	}
 
 	for (f = acpu_freq_tbl; f->acpuclk_khz != 0; f++)
