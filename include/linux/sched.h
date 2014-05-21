aa064093b32bc5a386de8d441dabe262b78506c7
Kees Cook <keescook@chromium.org>
2014-05-21 15:23:46 -0700

    sched: move no_new_privs into new atomic flags
    Since seccomp transitions between threads requires updates to the
    no_new_privs flag to be atomic, the flag must be part of an atomic flag
    set. This moves the nnp flag into a separate task field, and introduces
    accessors.
    
    Change-Id: I479b0a4c5980dd2ad55634d68eec9ffeab8a9271
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Oleg Nesterov <oleg@redhat.com>
    Reviewed-by: Andy Lutomirski <luto@amacapital.net>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1364,8 +1364,6 @@ struct task_struct {
 #endif
 	unsigned long atomic_flags; /* Flags needing atomic access. */
 
-	unsigned long atomic_flags; /* Flags needing atomic access. */
-
 	pid_t pid;
 	pid_t tgid;
 
