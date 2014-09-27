d611de67cf51c4bcea68f3a9c17f508f322cf247
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-09-27 12:10:51 +0300

    msm_fb: Suppress a log message.
    It's way too intrusive when switching the display on.
    
    Change-Id: Ie65179696ddb4fb238f03b659c73b06a772d2dfc

--- drivers/video/msm/mdp4_util.c
+++ drivers/video/msm/mdp4_util.c
@@ -2001,7 +2001,7 @@ int mdp4_csc_config(struct mdp_csc_cfg_data *config)
 
 	base = mdp4_csc_block2base(config->block);
 	if (!base) {
-		pr_warn("%s: Block type %d isn't supported by CSC.\n",
+		pr_debug("%s: Block type %d isn't supported by CSC.\n",
 				__func__, config->block);
 		return -EINVAL;
 	}
