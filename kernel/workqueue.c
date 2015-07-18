b2f60dfc8a5f68d24936f9cead661b8f7856567d
shumash <shumashgeely@gmail.com>
2015-07-18 09:03:08 -0600

    workqueues: Introduce new flag WQ_POWER_EFFICIENT for power oriented workqueues
    Workqueues can be performance or power-oriented. Currently, most workqueues are
    bound to the CPU they were created on. This gives good performance (due to cache
    effects) at the cost of potentially waking up otherwise idle cores (Idle from
    scheduler's perspective. Which may or may not be physically idle) just to
    process some work. To save power, we can allow the work to be rescheduled on a
    core that is already awake.
    
    Workqueues created with the WQ_UNBOUND flag will allow some power savings.
    However, we don't change the default behaviour of the system.  To enable
    power-saving behaviour, a new config option CONFIG_WQ_POWER_EFFICIENT needs to
    be turned on. This option can also be overridden by the
    workqueue.power_efficient boot parameter.
    
    tj: Updated config description and comments.  Renamed
        CONFIG_WQ_POWER_EFFICIENT to CONFIG_WQ_POWER_EFFICIENT_DEFAULT.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Amit Kucheria <amit.kucheria@linaro.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    (cherry picked from commit cee22a15052faa817e3ec8985a28154d3fabc7aa)
    Signed-off-by: Mark Brown <broonie@linaro.org>
    
    Change-Id: I5c2f656aaa266deba2dd0887dace9928540910ae

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -262,6 +262,15 @@ struct workqueue_struct {
 	char			name[];		/* I: workqueue name */
 };
 
+/* see the comment above the definition of WQ_POWER_EFFICIENT */
+#ifdef CONFIG_WQ_POWER_EFFICIENT_DEFAULT
+static bool wq_power_efficient = true;
+#else
+static bool wq_power_efficient;
+#endif
+
+module_param_named(power_efficient, wq_power_efficient, bool, 0644);
+
 struct workqueue_struct *system_wq __read_mostly;
 struct workqueue_struct *system_long_wq __read_mostly;
 struct workqueue_struct *system_nrt_wq __read_mostly;
@@ -3015,6 +3024,10 @@ struct workqueue_struct *__alloc_workqueue_key(const char *fmt,
 	va_list args, args1;
 	struct workqueue_struct *wq;
 	unsigned int cpu;
+	/* see the comment above the definition of WQ_POWER_EFFICIENT */
+	if ((flags & WQ_POWER_EFFICIENT) && wq_power_efficient)
+		flags |= WQ_UNBOUND;
+
 	size_t namelen;
 
 	/* determine namelen, allocate wq and format name */
